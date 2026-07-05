4cdcfa035332f148abf77c153ccb01f72b7953cb
ForeverFlank <113618855+ForeverFlank@users.noreply.github.com>
2026-07-05 14:36:33 +0700

    bringing back the scc + NodeId type

--- fog/src/optimizer/optimizer.rs
+++ fog/src/optimizer/optimizer.rs
@@ -11,54 +11,64 @@ use crate::parser::core_expr::DesugaredMatchArm;
 
 #[derive(Clone, PartialEq, Eq, Hash)]
 enum Node {
-    Stmt(usize),  // a top-level statement, by index in its block
-    Name(String), // a bound variable/type name
-    Expr(u32),    // an expression
+    Statement(usize), // a top-level statement, by index in its block
+    Name(String),     // a bound variable/type name
+    Expr(u32),        // an expression
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct NodeId(usize);
+
+impl NodeId {
+    fn value(self) -> usize {
+        self.0
+    }
 }
 
 // --- union-find ---
 
-struct DisjointSet {
-    parent: Vec<usize>,
+struct DSU {
+    parent: Vec<NodeId>,
     rank: Vec<usize>,
 }
 
-impl DisjointSet {
-    fn new() -> DisjointSet {
-        DisjointSet {
+impl DSU {
+    fn new() -> DSU {
+        DSU {
             parent: Vec::new(),
             rank: Vec::new(),
         }
     }
 
-    fn make_set(&mut self) -> usize {
-        let id = self.parent.len();
+    fn make_set(&mut self) -> NodeId {
+        let id = NodeId(self.parent.len());
         self.parent.push(id);
         self.rank.push(0);
+
         id
     }
 
-    fn find(&mut self, x: usize) -> usize {
-        if self.parent[x] != x {
-            self.parent[x] = self.find(self.parent[x]);
+    fn find(&mut self, x: NodeId) -> NodeId {
+        if self.parent[x.value()] != x {
+            self.parent[x.value()] = self.find(self.parent[x.value()]);
         }
 
-        self.parent[x]
+        self.parent[x.value()]
     }
 
-    fn union(&mut self, a: usize, b: usize) {
+    fn union(&mut self, a: NodeId, b: NodeId) {
         let (a, b) = (self.find(a), self.find(b));
 
         if a == b {
             return;
         }
 
-        match self.rank[a].cmp(&self.rank[b]) {
-            Ordering::Less => self.parent[a] = b,
-            Ordering::Greater => self.parent[b] = a,
+        match self.rank[a.value()].cmp(&self.rank[b.value()]) {
+            Ordering::Less => self.parent[a.value()] = b,
+            Ordering::Greater => self.parent[b.value()] = a,
             Ordering::Equal => {
-                self.parent[b] = a;
-                self.rank[a] += 1;
+                self.parent[b.value()] = a;
+                self.rank[a.value()] += 1;
             }
         }
     }
@@ -66,54 +76,140 @@ impl DisjointSet {
 
 // --- dependency graph ---
 
-struct GraphBuilder {
-    dsu: DisjointSet,
-    id_by_key: HashMap<Node, usize>,
-    edges: Vec<(usize, usize)>,
+struct DependencyGraph {
+    dsu: DSU,
+    id_by_node: HashMap<Node, NodeId>,
+    edges: Vec<(NodeId, NodeId)>,
     expr_id_counter: u32,
 }
 
-impl GraphBuilder {
-    fn new() -> GraphBuilder {
-        GraphBuilder {
-            dsu: DisjointSet::new(),
-            id_by_key: HashMap::new(),
-            expr_id_counter: 0,
+impl DependencyGraph {
+    fn new() -> DependencyGraph {
+        DependencyGraph {
+            dsu: DSU::new(),
+            id_by_node: HashMap::new(),
             edges: Vec::new(),
+            expr_id_counter: 0,
         }
     }
 
-    fn node(&mut self, key: Node) -> usize {
-        if let Some(&id) = self.id_by_key.get(&key) {
+    fn add_node(&mut self, node: Node) -> NodeId {
+        if let Some(&id) = self.id_by_node.get(&node) {
             return id;
         }
 
         let id = self.dsu.make_set();
-        self.id_by_key.insert(key, id);
+        self.id_by_node.insert(node, id);
 
         id
     }
 
-    fn fresh_expr_node(&mut self) -> usize {
+    fn add_stmt_node(&mut self, stmt_index: usize) -> NodeId {
+        self.add_node(Node::Statement(stmt_index))
+    }
+
+    fn add_name_node(&mut self, name: String) -> NodeId {
+        self.add_node(Node::Name(name))
+    }
+
+    fn add_expr_node(&mut self) -> NodeId {
         let id = self.expr_id_counter;
         self.expr_id_counter += 1;
 
-        self.node(Node::Expr(id))
+        self.add_node(Node::Expr(id))
     }
 
-    fn union(&mut self, a: usize, b: usize) {
+    fn union(&mut self, a: NodeId, b: NodeId) {
         self.dsu.union(a, b);
     }
 
-    fn add_edge(&mut self, scope: &Scope, from: usize, to_name: String) {
+    fn add_edge(&mut self, scope: &Scope, from: NodeId, to_name: String) {
         if scope.is_unbound(&to_name) {
             return;
         }
 
-        let to = self.node(Node::Name(to_name));
+        let to = self.add_node(Node::Name(to_name));
         self.edges.push((from, to));
     }
 }
+
+// Tarjan's SCC algorithm
+// https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
+
+fn tarjan_scc(adj: &Vec<Vec<NodeId>>) -> Vec<Vec<NodeId>> {
+    let node_count = adj.len();
+
+    let mut index = 0;
+    let mut indices = vec![usize::MAX; node_count];
+    let mut lowlinks = vec![usize::MAX; node_count];
+    let mut on_stack = vec![false; node_count];
+    let mut stack = Vec::new();
+
+    let mut all_sccs = Vec::new();
+
+    for v in 0..node_count {
+        let v = NodeId(v);
+        if indices[v.value()] == usize::MAX {
+            strong_connect(
+                v,
+                adj,
+                &mut index,
+                &mut indices,
+                &mut lowlinks,
+                &mut stack,
+                &mut on_stack,
+                &mut all_sccs,
+            );
+        }
+    }
+
+    all_sccs
+}
+
+fn strong_connect(
+    v: NodeId,
+    adj: &Vec<Vec<NodeId>>,
+    index: &mut usize,
+    indices: &mut Vec<usize>,
+    lowlinks: &mut Vec<usize>,
+    stack: &mut Vec<NodeId>,
+    on_stack: &mut Vec<bool>,
+    all_sccs: &mut Vec<Vec<NodeId>>,
+) {
+    indices[v.value()] = *index;
+    lowlinks[v.value()] = *index;
+    *index += 1;
+
+    stack.push(v);
+    on_stack[v.value()] = true;
+
+    for &w in &adj[v.value()] {
+        if indices[w.value()] == usize::MAX {
+            strong_connect(w, adj, index, indices, lowlinks, stack, on_stack, all_sccs);
+            lowlinks[v.value()] = lowlinks[v.value()].min(lowlinks[w.value()]);
+        } else if on_stack[w.value()] {
+            lowlinks[v.value()] = lowlinks[v.value()].min(indices[w.value()]);
+        }
+    }
+
+    if lowlinks[v.value()] == indices[v.value()] {
+        let mut scc = Vec::new();
+
+        loop {
+            let w = stack.pop().unwrap();
+            on_stack[w.value()] = false;
+            scc.push(w);
+
+            if v == w {
+                break;
+            }
+        }
+
+        all_sccs.push(scc);
+    }
+}
+
+// --- scope ---
 
 struct Scope<'a> {
     parent: Option<&'a Scope<'a>>,
@@ -206,14 +302,13 @@ fn optimize_block(mut stmts: Vec<CoreStatement>) -> Result<Vec<CoreStatement>, V
 
     // build a graph
     let scope = Scope::root();
-    let mut graph_builder = GraphBuilder::new();
+    let mut graph = DependencyGraph::new();
 
     for (index, stmt) in stmts.iter().enumerate() {
-        visit_stmt(&mut graph_builder, &scope, index, stmt);
+        visit_stmt(&mut graph, &scope, index, stmt);
     }
 
-    // toposort
-    // etc
+    // SCC
 
     if errors.is_empty() {
         Ok(optimized_stmts)
@@ -222,61 +317,64 @@ fn optimize_block(mut stmts: Vec<CoreStatement>) -> Result<Vec<CoreStatement>, V
     }
 }
 
-fn visit_stmt(builder: &mut GraphBuilder, scope: &Scope, index: usize, stmt: &CoreStatement) {
-    let stmt_node = builder.node(Node::Stmt(index));
+fn visit_stmt(graph: &mut DependencyGraph, scope: &Scope, index: usize, stmt: &CoreStatement) {
+    let stmt_node = graph.add_node(Node::Statement(index));
 
     match stmt {
         CoreStatement::TypeAnnotation { name, expr, .. } => {
-            let name_node = builder.node(Node::Name(name.clone()));
-            builder.union(stmt_node, name_node);
+            let name_node = graph.add_name_node(name.clone());
+            graph.union(stmt_node, name_node);
 
-            visit_expr(builder, scope, stmt_node, expr);
+            visit_expr(graph, scope, stmt_node, expr);
         }
 
         CoreStatement::Declaration { pattern, expr, .. } => {
             for name in pattern.all_identifiers() {
-                let name_node = builder.node(Node::Name(name.to_string()));
-                builder.union(stmt_node, name_node);
+                let name_node = graph.add_name_node(name.to_string());
+                graph.union(stmt_node, name_node);
             }
 
-            visit_expr(builder, scope, stmt_node, expr);
+            visit_expr(graph, scope, stmt_node, expr);
         }
 
         CoreStatement::Expression { expr, .. } => {
-            visit_expr(builder, scope, stmt_node, expr);
+            visit_expr(graph, scope, stmt_node, expr);
         }
     }
 }
 
-fn visit_stmt_expr(builder: &mut GraphBuilder, scope: &Scope, parent: usize, stmt: &CoreStatement) {
+fn visit_stmt_expr(
+    graph: &mut DependencyGraph,
+    scope: &Scope,
+    parent: NodeId,
+    stmt: &CoreStatement,
+) {
     let expr = match stmt {
         CoreStatement::TypeAnnotation { expr, .. }
         | CoreStatement::Declaration { expr, .. }
         | CoreStatement::Expression { expr, .. } => expr,
     };
 
-    visit_expr(builder, scope, parent, expr);
+    visit_expr(graph, scope, parent, expr);
 }
 
 fn visit_expr<'a>(
-    builder: &mut GraphBuilder,
+    graph: &mut DependencyGraph,
     scope: &'a Scope<'a>,
-    parent: usize,
+    parent: NodeId,
     expr: &CoreExpr,
 ) {
-    let this = builder.fresh_expr_node();
-    builder.union(parent, this);
+    let this = graph.add_expr_node();
+    graph.union(parent, this);
 
     match expr {
+        CoreExpr::Identifier { name, .. } => graph.add_edge(scope, this, name.clone()),
+
         CoreExpr::Block { statements, .. } => {
             for stmt in statements {
-                visit_stmt_expr(builder, scope, this, stmt);
+                visit_stmt_expr(graph, scope, this, stmt);
             }
         }
-
-        CoreExpr::Identifier { name, .. } => builder.add_edge(scope, this, name.clone()),
-
-        CoreExpr::Literal { .. } => {}
 
         CoreExpr::Lambda {
             param_name,
@@ -284,24 +382,25 @@ fn visit_expr<'a>(
             body,
             ..
         } => {
-            visit_expr(builder, scope, this, param_type);
+            visit_expr(graph, scope, this, param_type);
 
             let unbound = HashSet::from([param_name.clone()]);
             let inner_scope = scope.child(unbound);
-            visit_expr(builder, &inner_scope, this, body);
+
+            visit_expr(graph, &inner_scope, this, body);
         }
 
         CoreExpr::Tuple { items, .. } => {
             for item in items {
-                visit_expr(builder, scope, this, item);
+                visit_expr(graph, scope, this, item);
             }
         }
 
         CoreExpr::FunctionAppl { fn_name, args, .. } => {
-            builder.add_edge(scope, this, fn_name.clone());
+            graph.add_edge(scope, this, fn_name.clone());
 
             for arg in args {
-                visit_expr(builder, scope, this, arg);
+                visit_expr(graph, scope, this, arg);
             }
         }
 
@@ -310,19 +409,21 @@ fn visit_expr<'a>(
             match_arms,
             ..
         } => {
-            visit_expr(builder, scope, this, scrutinee);
+            visit_expr(graph, scope, this, scrutinee);
 
             for match_arm in match_arms {
-                visit_match_arm(builder, scope, this, match_arm);
+                visit_match_arm(graph, scope, this, match_arm);
             }
         }
+
+        CoreExpr::Literal { .. } => {}
     }
 }
 
 fn visit_match_arm<'a>(
-    builder: &mut GraphBuilder,
+    graph: &mut DependencyGraph,
     scope: &'a Scope<'a>,
-    parent: usize,
+    parent: NodeId,
     match_arm: &DesugaredMatchArm,
 ) {
     let unbound: HashSet<String> = match_arm
@@ -332,5 +433,6 @@ fn visit_match_arm<'a>(
         .collect();
 
     let inner_scope = scope.child(unbound);
-    visit_expr(builder, &inner_scope, parent, &match_arm.value_expr);
+
+    visit_expr(graph, &inner_scope, parent, &match_arm.value_expr);
 }
